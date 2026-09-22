d40d376d5a914e31da0eca5b58b7ba7c30151a91
ITGuyFromIA2 <Tanner@NetSysPlus.com>
2026-09-22 13:22:24 -0500

    Fix CodeSigning cert generation (no CA Flag)

--- Public/New-NSPCodeSigningCertificate.ps1
+++ Public/New-NSPCodeSigningCertificate.ps1
@@ -71,7 +71,7 @@ function New-NSPCodeSigningCertificate {
             -KeyExportPolicy ExportableEncrypted `
             -NotBefore $notBefore `
             -NotAfter $notAfter `
-            -TextExtension @('2.5.29.37={text}1.3.6.1.5.5.7.3.3', '2.5.29.19={critical}{text}ca=TRUE') `
+            -TextExtension @('2.5.29.37={text}1.3.6.1.5.5.7.3.3', '2.5.29.19={critical}{text}ca=FALSE') `
             -ErrorAction Stop
 
         foreach ($storeName in @('Root', 'TrustedPublisher')) {
